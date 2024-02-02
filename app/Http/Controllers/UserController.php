ee82645f9f736bd642be570f63c9aa02b33e027c
tonigith <91883442+toniGitH@users.noreply.github.com>
2024-02-02 13:26:58 +0100

    métodos register, login, logout y update del UserController funcionando + requests de login, register y update + rutas correspondientes

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\LoginUserRequest;
+use App\Http\Requests\RegisterUserRequest;
+use App\Http\Requests\UpdateUserRequest;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+class UserController extends Controller
+{
+
+    public function register(RegisterUserRequest $request)
+    {
+        $validatedData = $request->all();
+        $validatedData['password'] = Hash::make($request->password);
+        $user= User::create([
+            'name'=>$request->name ? $request->name : 'anonimo',
+            'email'=>$request->email,
+            'password'=>bcrypt($request->password)
+        ]); // PENDIENTE DE ASIGACIÓN DE ROLES ->assignRole('Pplayer');
+        $token = $user->createToken('authToken')->accessToken;
+        return response([
+            'message' => 'User ' . ucfirst($user->name) . ' registered successfully',
+            'user' => $user,
+            'token' => $token
+        ]);
+    }
+
+    public function login(LoginUserRequest $request)
+    {
+        $user = User::where('email', $request->email)->first();
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return response()->json([
+                'message' => 'The provided credentials are incorrect.'
+            ], 422);
+        }
+        $token = $user->createToken('authToken')->accessToken;
+        return response([
+            'message' => 'User ' . ucfirst($user->name) . ' logged successfully',
+            'token' => $token,
+            'user' => $user
+        ]);
+    }
+
+    public function logout()
+    {
+        $user=Auth::user();
+        $user->tokens->each->revoke();
+        return response()->json([
+            'message' => 'User ' . ucfirst($user->name) . ' logged out successfully'
+        ]);
+    }
+
+    public function update(UpdateUserRequest $request, $id)
+    {
+        $authUserId = Auth::user()->id;
+
+        if ($authUserId != $id) {
+            return response()->json(['error' => 'Unauthorized'], 403);
+        }
+
+        $user = Auth::user();
+        User::where('id', $user->id)->update(['name'=>$request->name ? $request->name : 'anonimo']);
+        return response()->json([
+            'message' => 'Username updated successfully',
+            'new name' => $request->name ? $request->name : 'anonimo'
+        ]);
+    }
+
+
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// RUTA PARA REGISTRAR UN USUARIO:
+Route::post('/register', [UserController::class, 'register']);
+
+// RUTA PARA LOGEAR A UN USUARIO YA REGISTRADO:
+Route::post('/login', [UserController::class, 'login']);
+
+// RUTA PARA DESLOGEAR A UN USUARIO LOGEADO
+Route::post('logout', [UserController::class, 'logout']);
+
+// RUTA PARA ACTUALIZAR EL NOMBRE DEL USUARIO
+Route::put('/user/{id}', [UserController::class, 'update']);
+/* Route::put('/user/{note}', [UserController::class, 'update'])->middleware('auth:api'); */
+
+
