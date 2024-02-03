d32a3a08d9b90b0189305fea3ef053a274c756fd
tonigith <91883442+toniGitH@users.noreply.github.com>
2024-02-03 20:56:04 +0100

    Update api routes file

--- routes/api.php
+++ routes/api.php
@@ -16,39 +16,48 @@ use App\Http\Controllers\RollController;
 |
 */
 
-// ROUTE TO REGISTER A PLAYER
-Route::post('/players', [UserController::class, 'register']); // NO SE PROTEGE => ESTÁ OK
+    // ROUTE TO REGISTER A PLAYER
+Route::post('/players', [UserController::class, 'register']);
 
-// ROUTE TO LOGIN A REGISTERED PLAYER
-Route::post('/login', [UserController::class, 'login']); // NO SE PROTEGE => ESTÁ OK
+    // ROUTE TO LOGIN A REGISTERED USER (VALID FOR ADMIN & PLAYERS)
+Route::post('/login', [UserController::class, 'login']);    
 
-// ROUTE TO LOGOUT A LOGGED LAYER
-Route::post('/logout', [UserController::class, 'logout'])->middleware('auth:api'); // SE PROTEGE CON PASSPORT
+    // ROUTES PROTECTED WITH PASSPORT
+Route::middleware('auth:api')->group(function () {
 
-// ROUTE TO UPDATE THE NAME OF A REGISTERED PLAYER/USER (VALID FOR ADMIN & PLAYERS)
-Route::put('/players/{id}', [UserController::class, 'update'])->middleware('auth:api'); // PROTEGIDA => ESTÁ OK
+        // ROUTE TO LOGOUT A LOGGED LAYER
+    Route::post('/logout', [UserController::class, 'logout']); // Grant permission to: All
 
-// ROUTE TO GET THE ALL PLAYER LIST WITH WIN RATE
-Route::get('/players', [UserController::class, 'index'])->middleware('auth:api'); // SOLO PERMITIR A ROLL ADMIN => PENDIENTE
+        // ROUTE TO UPDATE THE NAME OF A REGISTERED PLAYER/USER (VALID FOR ADMIN & PLAYERS)
+    Route::put('/players/{id}', [UserController::class, 'update']); // Grant permission to: All
 
-// ROUTE TO GET THE PLAYER RANKING ORDERED BY DESCENDING WIN RATE (FROM HIGHEST TO LOWEST)
-Route::get('/players/ranking', [UserController::class, 'ranking'])->middleware('auth:api'); // SOLO PERMITIR A ROLL ADMIN => PENDIENTE
+        // ROUTE TO GET THE ALL PLAYER LIST WITH WIN RATE
+    Route::get('/players', [UserController::class, 'index'])->middleware('can:players.index'); // Grant permission to: Admin
 
-// ROUTE TO GET THE PLAYER WITH HIGHEST RANKING
-Route::get('/players/ranking/winner', [UserController::class, 'winner'])->middleware('auth:api'); // SOLO PERMITIR A ROLL ADMIN => PENDIENTE
+        // ROUTE TO GET THE PLAYER RANKING ORDERED BY DESCENDING WIN RATE
+    Route::get('/players/ranking', [UserController::class, 'ranking'])->middleware('can:players.ranking'); // Grant permission to: Admin
 
-// ROUTE TO GET THE PLAYER WITH LOWEST RANKING
-Route::get('/players/ranking/loser', [UserController::class, 'loser'])->middleware('auth:api'); // SOLO PERMITIR A ROLL ADMIN => PENDIENTE
+        // ROUTE TO GET THE PLAYER WITH HIGHEST RANKING
+    Route::get('/players/ranking/winner', [UserController::class, 'winner'])->middleware('can:players.winner'); // Grant permission to: Admin
 
-// ROUTE TO EXECUTE A ROLL DICE OF A SPECIFIC PLAYER
-Route::post('/players/{id}/games', [RollController::class, 'store'])->middleware('auth:api'); // SOLO PERMITIR A ROLL PLAYER => PENDIENTE
+        // ROUTE TO GET THE PLAYER WITH LOWEST RANKING
+    Route::get('/players/ranking/loser', [UserController::class, 'loser'])->middleware('can:players.loser'); // Grant permission to: Admin
 
-// ROUTE TO DELETE ALL ROLLS OF A SPECIFIC PLAYER
-Route::delete('/players/{id}/games', [RollController::class, 'destroy'])->middleware('auth:api'); // SOLO PERMITIR A ROLL PLAYER => PENDIENTE
+        // ROUTE TO EXECUTE A ROLL DICE OF A SPECIFIC PLAYER
+    Route::post('/players/{id}/games', [RollController::class, 'store'])->middleware('can:players.store'); // Grant permission to: Player
 
-// ROUTE TO SHOW ALL ROLLS OF A SPECIFIC PLAYER
-Route::get('/players/{id}/games', [RollController::class, 'show'])->middleware('auth:api'); // SOLO PERMITIR A ROLL PLAYER => PENDIENTE
+        // ROUTE TO DELETE ALL ROLLS OF A SPECIFIC PLAYER
+    Route::delete('/players/{id}/games', [RollController::class, 'destroy'])->middleware('can:players.destroy'); // Grant permission to: Player
 
-// ROUTE TO SHOW THE WIN RATE OF A SPECIFIC PLAYER
-Route::get('/players/{id}/average', [RollController::class, 'getWinRate'])->middleware('auth:api'); // SOLO PERMITIR A ROLL PLAYER => PENDIENTE
+        // ROUTE TO SHOW ALL ROLLS OF A SPECIFIC PLAYER
+    Route::get('/players/{id}/games', [RollController::class, 'show'])->middleware('can:players.show'); // Grant permission to: Player
+
+        // ROUTE TO SHOW THE WIN RATE OF A SPECIFIC PLAYER
+    Route::get('/players/{id}/average', [RollController::class, 'getWinRate'])->middleware('can:players.getWinRate'); // Grant permission to: Player
+
+});
+
+
+
+
 
