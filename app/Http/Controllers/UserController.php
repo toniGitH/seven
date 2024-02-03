74e58b240447e8ed4d6ef5af21e5c829451b6b60
tonigith <91883442+toniGitH@users.noreply.github.com>
2024-02-03 20:57:18 +0100

    Added role assignation into method register

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -13,6 +13,61 @@ use Illuminate\Support\Facades\Auth;
 class UserController extends Controller
 {
 
+    public function register(RegisterUserRequest $request)
+    {
+        $validatedData = $request->all();
+        $validatedData['password'] = Hash::make($request->password);
+        $user= User::create([
+            'name'=>$request->name ? $request->name : 'anonimo',
+            'email'=>$request->email,
+            'password'=>bcrypt($request->password)
+        ])->assignRole('Player');
+        $token = $user->createToken('authToken')->accessToken;
+        return response([
+            'message' => 'User ' . ucfirst($user->name) . ' registered successfully',
+            'user' => $user,
+            'token' => $token
+        ]);
+    }
+
+    public function login(LoginUserRequest $request)
+    {
+        $user = User::where('email', $request->email)->first();
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return response()->json([
+                'message' => 'The provided credentials are incorrect.'
+            ], 422);
+        }
+        $token = $user->createToken('authToken')->accessToken;
+        return response([
+            'message' => 'User ' . ucfirst($user->name) . ' logged successfully',
+            'token' => $token,
+            'user' => $user
+        ]);
+    }
+
+    public function logout()
+    {
+        $user=Auth::user();
+        $user->tokens->each->revoke();
+        return response()->json([
+            'message' => 'User ' . ucfirst($user->name) . ' logged out successfully'
+        ]);
+    }
+
+    public function update(UpdateUserRequest $request, $id)
+    {
+        $userId = Auth::user()->id;
+        if ($userId != $id) {
+            return response()->json(['error' => 'Unauthorized'], 403);
+        }
+        User::where('id', $userId)->update(['name'=>$request->name ? $request->name : 'anonimo']);
+        return response()->json([
+            'message' => 'Username updated successfully',
+            'new name' => $request->name ? $request->name : 'anonimo'
+        ]);
+    }
+
     public function index()
     {
         $user = Auth::user();
@@ -129,62 +184,5 @@ class UserController extends Controller
             'loser' => $loser
         ]);
     }
-
-
-    public function register(RegisterUserRequest $request)
-    {
-        $validatedData = $request->all();
-        $validatedData['password'] = Hash::make($request->password);
-        $user= User::create([
-            'name'=>$request->name ? $request->name : 'anonimo',
-            'email'=>$request->email,
-            'password'=>bcrypt($request->password)
-        ]); // PENDIENTE DE ASIGACIÓN DE ROLES ->assignRole('Pplayer');
-        $token = $user->createToken('authToken')->accessToken;
-        return response([
-            'message' => 'User ' . ucfirst($user->name) . ' registered successfully',
-            'user' => $user,
-            'token' => $token
-        ]);
-    }
-
-    public function login(LoginUserRequest $request)
-    {
-        $user = User::where('email', $request->email)->first();
-        if (!$user || !Hash::check($request->password, $user->password)) {
-            return response()->json([
-                'message' => 'The provided credentials are incorrect.'
-            ], 422);
-        }
-        $token = $user->createToken('authToken')->accessToken;
-        return response([
-            'message' => 'User ' . ucfirst($user->name) . ' logged successfully',
-            'token' => $token,
-            'user' => $user
-        ]);
-    }
-
-    public function logout()
-    {
-        $user=Auth::user();
-        $user->tokens->each->revoke();
-        return response()->json([
-            'message' => 'User ' . ucfirst($user->name) . ' logged out successfully'
-        ]);
-    }
-
-    public function update(UpdateUserRequest $request, $id)
-    {
-        $userId = Auth::user()->id;
-        if ($userId != $id) {
-            return response()->json(['error' => 'Unauthorized'], 403);
-        }
-        User::where('id', $userId)->update(['name'=>$request->name ? $request->name : 'anonimo']);
-        return response()->json([
-            'message' => 'Username updated successfully',
-            'new name' => $request->name ? $request->name : 'anonimo'
-        ]);
-    }
-
 
 }
